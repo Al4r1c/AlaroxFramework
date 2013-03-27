8f1a9d444cc393bc23fe6e619cd6d7d0171e9e20
yann <yann.kurdijaka@wanadoo.fr>
2013-03-27 10:42:20 +0100

    Réorganisation de Conteneur

--- src/AlaroxFramework/Conteneur.php
+++ src/AlaroxFramework/Conteneur.php
@@ -2,6 +2,7 @@
 namespace AlaroxFramework;
 
 use AlaroxFileManager\AlaroxFile;
+use AlaroxFileManager\FileManager\File;
 use AlaroxFramework\cfg\Config;
 use AlaroxFramework\cfg\RouteMap;
 use AlaroxFramework\cfg\Server;
@@ -9,27 +10,49 @@ use AlaroxFramework\cfg\Server;
 class Conteneur
 {
     /**
-     * @param string $cheminVersFichier
-     * @param string $cheminVersRouteMap
-     * @return Config
+     * @param $cheminVersFichier
+     * @return File
      */
-    public function getConfig($cheminVersFichier, $cheminVersRouteMap)
+    public function getFile($cheminVersFichier)
     {
         $alaroxFile = new AlaroxFile();
 
+        return $alaroxFile->getFile($cheminVersFichier);
+    }
+
+    /**
+     * @param string $cheminVersFichierConfig
+     * @param string $cheminVersRouteMap
+     * @return Config
+     */
+    public function getConfig($cheminVersFichierConfig, $cheminVersRouteMap)
+    {
         $config = new Config();
-        $config->recupererConfigDepuisFichier($alaroxFile->getFile($cheminVersFichier));
+        $config->recupererConfigDepuisFichier($this->getFile($cheminVersFichierConfig));
         $config->parseServer($this->getServer());
 
         if (!empty($cheminVersRouteMap)) {
-            $routeMap = new RouteMap();
-            $routeMap->setRouteMapDepuisFichier($cheminVersRouteMap);
-            $config->setRouteMap($routeMap);
+            $config->setRouteMap($this->getRoute($cheminVersRouteMap));
         }
 
         return $config;
     }
 
+    /**
+     * @param $cheminVersRouteMap
+     * @return RouteMap
+     */
+    public function getRoute($cheminVersRouteMap)
+    {
+        $routeMap = new RouteMap();
+        $routeMap->setRouteMapDepuisFichier($this->getFile($cheminVersRouteMap));
+
+        return $routeMap;
+    }
+
+    /**
+     * @return Server
+     */
     public function getServer()
     {
         $server = new Server();
